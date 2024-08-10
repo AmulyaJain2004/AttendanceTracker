4e10c8c2250557a7faa11550c96b8563d290d11f
AmulyaJain <jainamulyawin@gmail.com>
2024-08-10 22:17:12 +0530

    made required changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 echo " BUILD START"
-python3.10 -m pip install --upgrade pip 
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python3.9  -m pip install -r req55jgbhkjkjkjkjkjkjhkhkjhkjhkjhkjhghikjugljhkgyugfhnvbcbhgfghdfhggthjgdjhgjghtuirements.txt
+python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
