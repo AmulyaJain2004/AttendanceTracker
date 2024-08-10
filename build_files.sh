72de9d0f904d50b45a730b6e9cf18fe0162ebcf0
AmulyaJain <jainamulyawin@gmail.com>
2024-08-10 22:06:57 +0530

    made required changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+python3.10 -m pip install --upgrade pip 
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo " BUILD END"
