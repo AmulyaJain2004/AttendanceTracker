afabd69c009907438f7d6a801005efbf4ad655f0
AmulyaJain <jainamulyawin@gmail.com>
2024-08-10 22:41:25 +0530

    made required changes

--- attendance_tracker/attendance_tracker/settings.py
+++ attendance_tracker/attendance_tracker/settings.py
@@ -133,4 +133,4 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
