a5c7b2b814c3b5010b9ba76fb3f5470ad9a046af
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-08-11 14:39:43 +0530

    Delete attendance_tracker directory

--- attendance_tracker/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from accounts.models import CustomUser
-
-# Register your models here.
-admin.site.register(CustomUser)
--- attendance_tracker/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home, name="home"),
-    path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name='signin'),
-    path('forgotpwd/', views.forgot_password, name='forgot_password'),
-]
